f1620775dfd6fef7026e8bb42e30be29584a2678
miriam590 <miriam.yego@student.moringaschool.com>
2025-03-12 00:04:16 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    """Write content to a .txt file."""
+    with open(f"{file_name}.txt", 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Append content to a .txt file."""
+    with open(f"{file_name}.txt", 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Read content from a .txt file."""
+    with open(f"{file_name}.txt", 'r') as file:
+        return file.read()
